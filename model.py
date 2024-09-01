b73211add552d6745019412beae501a659fc308f
Amee Assad <aa3628@columbia.edu>
2024-09-01 11:59:03 +0200

    resnet+2fc model

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-# https://github.com/karasawatakumi/pytorch-image-classification/blob/main/train.py
+# base from https://github.com/karasawatakumi/pytorch-image-classification/blob/main/train.py
 
 import os
 import numpy as np
@@ -142,6 +142,149 @@ class SimpleModel(LightningModule):
     
     def configure_optimizers(self):
         optimizer = get_optimizer(self.parameters())
+        lr_scheduler_config = get_lr_scheduler_config(optimizer)
+        return {"optimizer": optimizer, "lr_scheduler": lr_scheduler_config}
+    
+class ResNetPlus2FCModel(LightningModule):
+    def __init__(
+        self,
+        model_name: str = 'resnet18',
+        pretrained: bool = True, # Use ImageNet pre-trained weights
+        num_classes: int | None = None,
+        outdir: str = 'results',
+    ):
+        super().__init__()
+        self.save_hyperparameters()
+
+        self.model = timm.create_model(model_name=model_name, pretrained=pretrained, num_classes=0)  # No classification head yet
+
+        # Freeze the ResNet backbone
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+        # Add new fully connected layers
+        self.fc1 = nn.Linear(2048, 1024)  # ResNet-50 output features = 2048
+        self.fc2 = nn.Linear(1024, num_classes) 
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.5)  # Optional dropout for regularization
+
+        
+        self.train_loss = nn.CrossEntropyLoss()
+        self.train_acc = Accuracy(task='multiclass', num_classes=num_classes)
+        self.val_loss = nn.CrossEntropyLoss()
+        self.val_acc = Accuracy(task='multiclass', num_classes=num_classes)
+        self.gradient = None
+        self.outdir = outdir
+    
+    def activations_hook(self, grad):
+        self.gradient = grad
+
+    def get_gradient(self):
+        return self.gradient
+
+    def get_activations(self, x):
+        for name, module in self.model.named_modules():
+            if name == 'layer4':
+                x = module(x)
+                x.register_hook(self.activations_hook)
+                return x
+        return None
+
+    def forward(self, x):
+         # Forward pass through ResNet-50 backbone
+        features = self.model(x)
+        x = features.view(features.size(0), -1)  # Flatten the output
+        
+        # Fully connected layers
+        x = self.relu(self.fc1(x))
+        x = self.dropout(x)  # Optional
+        x = self.fc2(x)
+
+        return x
+
+    def training_step(self, batch, batch_idx):
+        x, target = batch
+
+        out = self(x)
+        _, pred = out.max(1)
+
+        loss = self.train_loss(out, target)
+        acc = self.train_acc(pred, target)
+        self.log_dict({'train/loss': loss, 'train/acc': acc}, prog_bar=True)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        if config['use_gradcam']:
+            with torch.enable_grad():
+                x, target = batch
+                out = self(x)
+                _, pred = out.max(1)
+
+                loss = self.val_loss(out, target)
+                acc = self.val_acc(pred, target)
+                self.log_dict({'val/loss': loss, 'val/acc': acc})
+
+                unnormalized_x = unnormalize(x[0].cpu(), config['transforms']['mean'], config['transforms']['std']).permute(1, 2, 0).numpy()
+                unnormalized_x = np.clip(unnormalized_x, 0, 1)  # Ensure the values are within [0, 1]
+
+
+                cam = GradCAM(model=self.model, target_layers=[self.model.layer4[-1]])
+                targets = [ClassifierOutputTarget(class_idx) for class_idx in target]
+                grayscale_cam = cam(input_tensor=x, targets=targets)
+                grayscale_cam = grayscale_cam[0, :]
+                visualization = show_cam_on_image(unnormalized_x, grayscale_cam, use_rgb=True)
+                img = Image.fromarray((visualization * 255).astype(np.uint8))
+
+                # Log image to 
+                if config['use_wandb']:
+                    wandb_img = wandb.Image(visualization, caption=f"GradCAM Batch {batch_idx} Image 0")
+                    self.logger.experiment.log({"GradCAM Images": wandb_img})
+
+                
+                # save locally
+                os.makedirs(self.outdir, exist_ok=True)
+                img.save(os.path.join(self.outdir, f'cam_image_val_batch{batch_idx}_img0.png'))
+                
+                # To save all images in batch:
+                # for i in range(len(x)):
+                #     grayscale_cam_img = 
+                # grayscale_cam[i]
+                #     visualization = show_cam_on_image(x[i].cpu().numpy().transpose(1, 2, 0), grayscale_cam_img, use_rgb=True)
+                #     img = Image.fromarray((visualization * 255).astype(np.uint8))
+                #     os.makedirs(self.hparams.outdir, exist_ok=True)
+                #     img.save(os.path.join(self.hparams.outdir, f'cam_image_val_batch{batch_idx}_img{i}.png'))
+                
+                # self.model.train()
+        else:
+            x, target = batch
+            out = self(x)
+            _, pred = out.max(1)
+
+            loss = self.val_loss(out, target)
+            acc = self.val_acc(pred, target)
+            self.log_dict({'val/loss': loss, 'val/acc': acc})
+
+    def test_step(self, batch, batch_idx):
+            x, target = batch
+
+            x = x.to(torch.device('cpu'))
+            target = target.to(torch.device('cpu'))
+
+            x.requires_grad = True
+        
+            out = self(x)
+
+            _, pred = out.max(1)
+            if pred.numel() == 1:
+                print(f"BATCH {batch_idx} PREDICTION: {pred.item()}")
+            else:
+                print(f"BATCH {batch_idx} PREDICTIONS: {pred.tolist()}")
+
+    
+    def configure_optimizers(self):
+        # Only optimize the parameters of the added fully connected layers
+        optimizer = get_optimizer(filter(lambda p: p.requires_grad, self.parameters()))
         lr_scheduler_config = get_lr_scheduler_config(optimizer)
         return {"optimizer": optimizer, "lr_scheduler": lr_scheduler_config}
     
