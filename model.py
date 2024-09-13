d11de47f54ee9942e268d99fa075417b5b9bd544
Amee Assad <aa3628@columbia.edu>
2024-09-13 20:07:49 +0200

    logging

--- model.py
+++ model.py
@@ -91,8 +91,6 @@ class SimpleModel(LightningModule):
 
         loss = self.train_loss(out, target)
         acc = self.train_acc(pred, target)
-
-        self.log('epoch', self.current_epoch, prog_bar=True, logger=True)
         # self.log_dict({'train/loss': loss, 'train/acc': acc}, prog_bar=True)
         self.log('train/loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         self.log('train/acc', acc, on_step=True, on_epoch=True, prog_bar=True, logger=True)
