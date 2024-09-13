26d13e70a57e37ad3359973d430dfaaf0689892f
Amee Assad <aa3628@columbia.edu>
2024-09-13 16:17:58 +0200

    logger

--- train.py
+++ train.py
@@ -111,7 +111,7 @@ def get_trainer(config) -> Trainer:
                                                 "lr_step_size": config['solver']['LR_STEP_SIZE'],
                                                 "lr_step_milestones": config['solver']['LR_STEP_MILESTONES']
                                                 })
-        # wandb_logger.watch(model, log='all', log_freq=10)
+        wandb_logger.watch(model, log='all', log_freq=10)
 
     else:
         wandb_logger = None
